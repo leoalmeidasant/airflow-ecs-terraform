41d8313341193d222acf72852ad25ad12a9d9e81
Fabricio Batista <fapbatista@gmail.com>
2020-10-15 19:46:16 -0300

    Change of bucket name

--- data/efs/main.tf
+++ data/efs/main.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket = "terraform-state-leoalmeida"
+    bucket = "terraform-state-airflow-letrus"
     key    = "network/vpc/terraform.tfstate"
     region = "us-east-1"
   }
@@ -10,7 +10,7 @@ data "terraform_remote_state" "network" {
 data "terraform_remote_state" "sg" {
   backend = "s3"
   config = {
-    bucket = "terraform-state-leoalmeida"
+    bucket = "terraform-state-airflow-letrus"
     key    = "network/sg/terraform.tfstate"
     region = "us-east-1"
   }
--- data/efs/state.tf
+++ data/efs/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-leoalmeida"
+    bucket = "terraform-state-airflow-letrus"
     key    = "data/efs"
     region = "us-east-1"
   }
